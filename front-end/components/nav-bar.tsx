07a5c388c5eeb359d4fde09a437949e4d5a18136
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-15 14:52:22 +0200

    feat: create nav-bar component [frontend]

---
+++ front-end/components/nav-bar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Inter } from "next/font/google";
+import classNames from "classnames";
+
+const interTight = Inter({ subsets: ["latin"], weight: ["700"] });
+
+export default function NavBar() {
+  return (
+    <div className="flex items-center justify-between border-b border-b-neutral-900/10 py-2 mb-2">
+      <Link href="/" className={classNames(interTight.className, "text-2xl")}>
+        Blog Ninja
+      </Link>
+      <div>
+        <nav className="flex gap-5">
+          <Link href="/blogs">Blogs</Link>
+          <Link href="/about">About</Link>
+          <Link href="/new-blog">New Blog</Link>
+        </nav>
+      </div>
+    </div>
+  );
+}

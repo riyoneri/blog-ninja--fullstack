301ab03c0ed3b505d6a317a30562a839e09f1962
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-15 17:55:28 +0200

    feat: connect to mongo database [backend]

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,8 +1,20 @@
+import { config } from "dotenv";
 import express from "express";
+import { connect } from "mongoose";
 import morgan from "morgan";
+import { exit } from "node:process";
+
+config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.listen(5000, () => console.log("[Server]: 5000"));
+if (MONGODB_URL) {
+  connect(MONGODB_URL)
+    .then(() => app.listen(5000, () => console.log("[Server]: 5000")))
+    // eslint-disable-next-line unicorn/prefer-top-level-await
+    .catch(() => exit(1));
+}

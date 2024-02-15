7721471a2b696b2f08a5d5b8d5f06c3eefe560f5
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-15 20:29:52 +0200

    feat: final steps of create-blog controller [backend]

--- back-end/controllers/blog-controller.ts
+++ back-end/controllers/blog-controller.ts
@@ -1,7 +1,42 @@
 import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { CustomError, serializeValidation } from "../util";
+import Blog from "../models/blog-model";
 
 export async function createBlog(
   request: Request,
   response: Response,
   next: NextFunction,
-) {}
+) {
+  try {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      const error = new CustomError(
+        "Validation failed",
+        400,
+        serializeValidation(errors),
+      );
+
+      return next(error);
+    }
+
+    const blogData = new Blog(request.body);
+
+    const savedBlog = await blogData.save();
+
+    const blog = savedBlog.toJSON({
+      transform(document, returnValue) {
+        delete returnValue._id;
+        return {
+          _id: document._id,
+          ...returnValue,
+        };
+      },
+    });
+
+    response.status(200).json(blog);
+  } catch {
+    const error = new CustomError("Internal server error", 500);
+    next(error);
+  }
+}

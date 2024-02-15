01e3139a4df59f694c245d180055a0c01a4fd893
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-15 20:06:26 +0200

    feat: validate create-blog route [backend]

--- back-end/routes/blog-routes.ts
+++ back-end/routes/blog-routes.ts
@@ -1,8 +1,29 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import * as blogController from "../controllers/blog-controller";
 
 const router = Router();
 
-router.post("/blogs", blogController.createBlog);
+router.post(
+  "/",
+  [
+    body("title", "Blog title is required")
+      .notEmpty({
+        ignore_whitespace: true,
+      })
+      .isString(),
+    body("snippet", "Blog snippet is required")
+      .notEmpty({
+        ignore_whitespace: true,
+      })
+      .isString(),
+    body("body", "Blog body is required")
+      .notEmpty({
+        ignore_whitespace: true,
+      })
+      .isString(),
+  ],
+  blogController.createBlog,
+);
 
 export default router;

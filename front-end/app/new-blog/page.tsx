814afb0ba46d553c1f46311f7b2b66c648638af6
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-16 18:05:40 +0200

    feat: collect form data [frontend]

--- front-end/app/new-blog/page.tsx
+++ front-end/app/new-blog/page.tsx
@@ -1,21 +1,51 @@
 "use client";
 
 import Title from "@/components/title";
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function NewBlog() {
+  const [formValues, setFormValues] = useState({
+    title: "",
+    snippet: "",
+    body: "",
+  });
+
+  function handleInputChange(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    const { target } = event;
+    setFormValues((previousFormValues) => ({
+      ...previousFormValues,
+      [target.name]: target.value,
+    }));
+  }
+
+  function handleFormSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(formValues);
+  }
+
   return (
     <>
       <Title title="Add Blog" />
+      <div className="mx-auto max-w-xl w-full md:w-1/2 flex flex-col text-sm gap-5 bg-red-800/20 p-3 rounded-md mb-10">
+        <div className="flex items-center gap-3">
+          <span className="w-3 h-0.5 bg-red-400"></span>
+          <span>This is validation error</span>
+        </div>
+      </div>
       <form
-        className="mx-auto max-w-xl md:w-1/2 flex flex-col gap-5 *:flex *:flex-col"
-        onSubmit={(event: FormEvent) => event.preventDefault()}
+        className="mx-auto max-w-xl md:w-1/2 w-full flex flex-col gap-5 *:flex *:flex-col"
+        onSubmit={handleFormSubmit}
       >
         <label>
           <p>Blog title:</p>
           <input
             type="text"
             name="title"
+            onChange={handleInputChange}
+            value={formValues.title}
             className="border border-neutral-800 rounded-sm focus:outline-none p-1"
           />
         </label>
@@ -24,13 +54,17 @@ export default function NewBlog() {
           <input
             type="text"
             name="snippet"
+            onChange={handleInputChange}
+            value={formValues.snippet}
             className="border border-neutral-800 rounded-sm focus:outline-none p-1"
           />
         </label>
         <label>
           <p>Blog body:</p>
           <textarea
-            name=""
+            name="body"
+            onChange={handleInputChange}
+            value={formValues.body}
             className="border border-neutral-800 rounded-sm focus:outline-none p-1"
           ></textarea>
         </label>

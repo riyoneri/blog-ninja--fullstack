11c7747e056ebaf724901bab9ccbeb34f6d0ddfc
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-16 14:25:08 +0200

    feat: initialize react-query globa provider [frontend]

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,33 +1,36 @@
+"use client";
+
 import NavBar from "@/components/nav-bar";
 import classNames from "classnames";
-import type { Metadata } from "next";
+import { useState } from "react";
 import { Questrial } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./globals.css";
 import Footer from "@/components/footer";
 
 const questrial = Questrial({ subsets: ["latin"], weight: "400" });
-
-export const metadata: Metadata = {
-  title: "Blog Ninja",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <html lang="en">
-      <body
-        className={classNames(
-          questrial.className,
-          "max-w-screen-2xl min-h-screen px-5 sm:px-10 mx-auto flex flex-col py-3",
-        )}
-      >
-        <NavBar />
-        <main className="flex-1 flex flex-col">{children}</main>
-        <Footer />
-      </body>
+      <QueryClientProvider client={queryClient}>
+        <body
+          className={classNames(
+            questrial.className,
+            "max-w-screen-2xl min-h-screen px-5 sm:px-10 mx-auto flex flex-col py-3",
+          )}
+        >
+          <NavBar />
+          <main className="flex-1 flex flex-col">{children}</main>
+          <Footer />
+        </body>
+      </QueryClientProvider>
     </html>
   );
 }

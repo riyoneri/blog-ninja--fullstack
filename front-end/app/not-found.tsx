41c70665edc43c785fb8aac577394ce682cf9c0c
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-16 08:35:51 +0200

    feat: create not-found page [frontend]

---
+++ front-end/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="grid place-content-center">
+        <p className="animate-bounce">Page not found!</p>
+      </div>
+    </>
+  );
+}

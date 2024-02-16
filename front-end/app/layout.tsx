dd20c3e09b01411608bd0e5f87ca39538a06b7c5
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-16 08:32:36 +0200

    refactor: add default grid to root-layout [frontend]

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         )}
       >
         <NavBar />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 grid">{children}</main>
         <Footer />
       </body>
     </html>

7270e4930a4737c0e7209a6944b0525182bc3aab
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-16 09:52:32 +0200

    feat: create get-all-blogs route [backend]

--- back-end/routes/blog-routes.ts
+++ back-end/routes/blog-routes.ts
@@ -4,26 +4,28 @@ import * as blogController from "../controllers/blog-controller";
 
 const router = Router();
 
-router.post(
-  "/",
-  [
-    body("title", "Blog title is required")
-      .notEmpty({
-        ignore_whitespace: true,
-      })
-      .isString(),
-    body("snippet", "Blog snippet is required")
-      .notEmpty({
-        ignore_whitespace: true,
-      })
-      .isString(),
-    body("body", "Blog body is required")
-      .notEmpty({
-        ignore_whitespace: true,
-      })
-      .isString(),
-  ],
-  blogController.createBlog,
-);
+router
+  .post(
+    "/",
+    [
+      body("title", "Blog title is required")
+        .notEmpty({
+          ignore_whitespace: true,
+        })
+        .isString(),
+      body("snippet", "Blog snippet is required")
+        .notEmpty({
+          ignore_whitespace: true,
+        })
+        .isString(),
+      body("body", "Blog body is required")
+        .notEmpty({
+          ignore_whitespace: true,
+        })
+        .isString(),
+    ],
+    blogController.createBlog,
+  )
+  .get("/", blogController.getAllBlogs);
 
 export default router;

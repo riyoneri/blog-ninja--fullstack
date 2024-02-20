d4bdc15247a0b3dd71a372edc41b28ce0253b3d1
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-20 13:02:36 +0200

    feat: fetch delete blog [frontend]

--- front-end/app/blogs/[id]/page.tsx
+++ front-end/app/blogs/[id]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Loader from "@/components/loader";
 import ResponseCard from "@/components/response-card";
 import Title from "@/components/title";
 import { BlogDto } from "@/util/api";
-import { singleBlog } from "@/util/fetcher";
-import { useQuery } from "@tanstack/react-query";
-import { notFound, useParams } from "next/navigation";
+import { singleBlog, deleteBlog } from "@/util/fetcher";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { notFound, redirect, useParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function BlogDetails() {
   const { id } = useParams<{ id: string }>();
@@ -14,6 +16,19 @@ export default function BlogDetails() {
     queryKey: ["blog", id],
     queryFn: () => singleBlog(id),
   });
+
+  const {
+    data: deleteBlogData,
+    error: deleteBlogError,
+    isPending: deleteBlogPending,
+    mutate,
+  } = useMutation({
+    mutationFn: (id: string) => deleteBlog(id),
+  });
+
+  useEffect(() => {
+    if (deleteBlogData) redirect("/blogs");
+  }, [deleteBlogData]);
 
   if (error && error.includes("404")) return notFound();
 
@@ -24,12 +39,22 @@ export default function BlogDetails() {
 
       {data && (
         <>
-          <div className="flex justify-between items-start cursor-pointer p-1">
+          <div className="flex justify-between items-start p-1">
             <Title title={data.title} />
-            <span className="rounded-full hover:shadow-md">delete</span>
+            {deleteBlogPending ? (
+              <Loader />
+            ) : (
+              <span
+                className="rounded-full hover:shadow-md cursor-pointer"
+                onClick={() => mutate(id)}
+              >
+                delete
+              </span>
+            )}
           </div>
           <div className="py-5">{data.snippet}</div>
           <div>{data.body}</div>
+          {deleteBlogError && <div>{deleteBlogError.message}</div>}
         </>
       )}
     </>
